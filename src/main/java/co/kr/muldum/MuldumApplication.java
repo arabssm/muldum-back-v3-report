2a03ad247804b4313913bc63750617b36dd34710
hxeonxins <hyunjin.dev143@gmail.com>
2025-11-09 22:37:24 +0900

    rename: change MuldumReportApplication to MuldumApplication

--- src/main/java/co/kr/muldum/MuldumApplication.java
+++ src/main/java/co/kr/muldum/MuldumApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MuldumReportApplication {
+public class MuldumApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MuldumReportApplication.class, args);
+        SpringApplication.run(MuldumApplication.class, args);
     }
 
 }
